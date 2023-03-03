997cc2a4fc9d769a89a27bf2abbf921017930ca0
ailiin24 <94693186+ailiin24@users.noreply.github.com>
2023-03-03 14:17:22 -0300

    initial commit

---
+++ javascript.js
@@ -0,0 +1,22 @@
+
+  document.getElementById("telefono").onclick = function() {
+
+    document.getElementById("telefono").innerHTML = "+54235-6548562"
+  }
+
+
+  document.getElementById("direccion").onclick = function() {
+
+     document.getElementById("direccion").innerHTML = "Av. San Juan 2536"
+  }
+
+document.getElementById("mail").onclick = function() {
+
+  document.getElementById("mail").innerHTML = "mimail@yahoo.com.ar"
+}
+
+document.getElementById("url").onclick = function() {
+
+document.getElementById("url").innerHTML = "www.mipaginaweb.com.ar"
+
+}
